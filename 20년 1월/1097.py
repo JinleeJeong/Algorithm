46695100ea9ffb91ccc0ef80366105bd7db6b456
JinleeJeong <pjjr0118@gmail.com>
2020-01-28 18:06:35 +0900

    m

--- 20년 1월/1097.py
+++ 20년 1월/1097.py
@@ -13,13 +13,14 @@ for i in range(0, n):
 
     for x in range(0, 19):
         for y in range(0, 19):
-
+            # x 값이 changeTime[0] 값 즉 바꾸는 행의 값과 같으면 반대 값을 넣어준다(뒤집기)
             if(x == (changeTime[0]-1)):
 
                 if(stage[x][y] == 1):
                     stage[x][y] = 0
                 else:
                     stage[x][y] = 1
+            # y 값이 changeTime[1] 값 즉 바꾸는 열의 값과 같으면 반대 값을 넣어준다(뒤집기)
             if(y == (changeTime[1]-1)):
 
                 
@@ -30,6 +31,7 @@ for i in range(0, n):
 
 for i in range(0, 19):
     for j in range(0, 19):
+        # 19개를 출력 후, 줄 바꿈 Prettier
         if(i > count):
             print("")
             count += 1
