1a3ddd18aaa3e9142eed42f43072fdc3700aa2a4
JinleeJeong <pjjr0118@gmail.com>
2020-01-30 15:04:53 +0900

    save

--- 20년 1월/1163.py
+++ 20년 1월/1163.py
@@ -3,8 +3,7 @@ a,b,c = map(int, input().split(" "))
 # 천의 자리를 100으로 나누면(떨어지는 값 생략) 1500 > 15가 되고, 거기서 2로 나눠서 짝수 홀수 비교
 result = ((a+b+c) // 100) % 2
 
-
-if(result == 1):
-    print("그럭저럭")
-else:
+if(result != 1):
     print("대박")
+else:
+    print("그럭저럭")
