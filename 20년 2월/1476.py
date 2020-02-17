43280f604d6544b87c585dd79c0b98bd85402e43
JinleeJeong <pjjr0118@gmail.com>
2020-02-17 15:24:49 +0900

    20.02.17 Algorithm commit

---
+++ 20년 2월/1476.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+matrix = [[0]*m for i in range(n)]
+count = 0
+
+#0부터 3까지 0~4
+for i in range(0, n+m-1): # 0 1 2 3 n+m-1인 이유는 2행 3열을 나타내기 때문에, 나열했을 때, i값과 같아야 함!!
+    for j in range(0, m): # 0 1 2
+        for k in range(0, n): # 0 1
+            if j+k == i:
+                
+                count += 1
+                matrix[k][j] = count
+
+for i in range(0, n):
+    for j in range(0, m):
+        print(matrix[i][j], end=' ')
+    print()
