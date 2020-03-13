6c1e946040bc240974d119eceacd717f37ee6eb7
JinleeJeong <pjjr0118@gmail.com>
2020-03-13 13:25:35 +0900

    20.03.12 Algorithm commit

--- 20년 3월/1715.py
+++ 20년 3월/1715.py
@@ -2,15 +2,12 @@ a, b = map(int, input().split())
 forFlag = True
 
 while True:
-    print("Entry")
+
     if a == 1 or forFlag == False:
-        print("??")
         break
     
     for i in range(2, a+1):
-        print(a, b, i)
         if a % i == 0 and b % i == 0:
-            print(a, b)
 
             a //= i
             b //= i
---
+++ 20년 3월/1716.py
@@ -0,0 +1,16 @@
+n, m = map(int,input().split())
+
+nSum = 0
+mSum = 30-m
+
+for i in range(n, 10):
+    if i >= 10:
+        break
+    else:
+        if i % 2 == 0:
+            nSum += 30
+        else:
+            nSum += 31
+
+
+print(nSum ,mSum)
