5383f55d9074b6af2f409742388e87f579fdea29
JinleeJeong <pjjr0118@gmail.com>
2020-03-31 14:56:19 +0900

    14:56 PM

---
+++ 20년 3월/2019.py
@@ -0,0 +1,20 @@
+import math
+
+a, b, c = map(int, input().split())
+
+check = b*b - 4 * a * c
+
+if check >= 0:
+    
+    print('%.2f' %( (-b + math.sqrt(check)) / (2 * a) ))
+    if check > 0:
+        print('%.2f' %( (-b - math.sqrt(check)) / (2 * a) ))
+else:
+
+    h = math.sqrt(-check) / (2 * a)
+    if h < 0:
+        print('%.2f+%.2fi' %(-b / (2*a), -h))
+        print('%.2f+%.2fi' %(-b / (2*a), h))
+    else:
+        print('%.2f+%.2fi' %(-b / (2*a), h))
+        print('%.2f%.2fi' %(-b / (2*a), -h))
