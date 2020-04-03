073b5edd839670d541b55a2fb3cc0dee59b69cb5
JinleeJeong <pjjr0118@gmail.com>
2020-04-03 11:22:36 +0900

    11:22 AM

---
+++ 20년 3월/2027.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+cornWay = [1, 1]
+
+for i in range(n-2):
+    idx = cornWay[len(cornWay)-1]
+    first = cornWay[idx-1]
+    second = cornWay[len(cornWay)-idx]
+
+    sumNum = first+second
+    cornWay.append(sumNum)
+    print(cornWay)
+
+print(cornWay[len(cornWay)-1])
