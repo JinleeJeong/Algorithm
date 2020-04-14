b6f15e14f47dc16666ce01d09992730e6fada198
JinleeJeong <pjjr0118@gmail.com>
2020-04-14 13:16:53 +0900

    20.04.14 Algorithm commit

--- 20년 3월/2026.py
+++
@@ -1,9 +0,0 @@
-hexi = list(input())
-
-strList = {'A' : 10, 'B' : 11, 'C' : 12, 'D' : 13, 'E' : 14, 'F' : 15}
-
-for i in range(0, len(hexi)):
-    if hexi[i] in strList:
-        print(format(strList.get(hexi[i]), 'b'), end=" ")
-    else:
-        print(format(int(hexi[i]), 'b').zfill(4), end=" ")
