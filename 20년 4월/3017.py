164bbc78dc17efc682ae505543edcebf5ec08066
JinleeJeong <pjjr0118@gmail.com>
2020-04-22 15:04:05 +0900

    15:04 PM

---
+++ 20년 4월/3017.py
@@ -0,0 +1,14 @@
+n = int(input())
+nList = []
+
+for i in range(n):
+    nList.append(list(map(int, input().split())))
+    nList[i].append(i+1)
+
+print(nList)
+nList = sorted(nList, key=lambda e: (-e[0], -e[1]))
+
+for i in nList:
+
+    print(i[2], i[0], i[1], end="")
+    print()
