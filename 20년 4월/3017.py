23fc03b918b25fde90ad51c268e83c1ae40d8c67
JinleeJeong <pjjr0118@gmail.com>
2020-04-22 15:10:33 +0900

    edit 3017.py

--- 20년 4월/3017.py
+++ 20년 4월/3017.py
@@ -5,7 +5,7 @@ for i in range(n):
     nList.append(list(map(int, input().split())))
     nList[i].append(i+1)
 
-print(nList)
+# print(nList)
 nList = sorted(nList, key=lambda e: (-e[0], -e[1]))
 
 for i in nList:
