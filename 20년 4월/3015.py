a81257d7e4a54a00ac2c9a1dd324cc7eeb765240
JinleeJeong <pjjr0118@gmail.com>
2020-05-12 16:16:08 +0900

    20.05.12 Algorithm commit

--- 20년 4월/3015.py
+++ 20년 4월/3015.py
@@ -7,8 +7,6 @@ for i in range(n):
     name, num = input().split()
     stuInfo[i] = {'name' : name, 'num' : int(num)}
     stuArr.append(stuInfo[i])
-print(stuArr)
-print(sorted(stuArr, key=lambda e: [-e['num']]))
 
 stuArr = sorted(stuArr, key=lambda e: [-e['num']])
 
